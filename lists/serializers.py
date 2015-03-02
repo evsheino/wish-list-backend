59cd8f15c16b5228242d45e06ff47600adfc3d9b
Erkki Heino <evsheino@gmail.com>
2015-03-02 15:22:47 +0200

    Made serializers hyperlinked; djangoframework-jwt

--- lists/serializers.py
+++ lists/serializers.py
@@ -3,25 +3,25 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from lists.models import Category, Gift, Purchase
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
 
-class PurchaseSerializer(serializers.ModelSerializer):
+class PurchaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Purchase
 
-class GiftSerializer(serializers.ModelSerializer):
+class GiftSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Gift
 
-class GiftWithPurchasesSerializer(serializers.ModelSerializer):
+class GiftWithPurchasesSerializer(serializers.HyperlinkedModelSerializer):
     purchases = PurchaseSerializer(many=True)
 
     class Meta:
         model = Gift
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name',)
+        fields = ('url', 'id', 'username', 'first_name', 'last_name')
--- wishlistsite/settings.py
+++ wishlistsite/settings.py
@@ -48,6 +48,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'wishlistsite.urls'
